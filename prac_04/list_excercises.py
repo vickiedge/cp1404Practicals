7c210fd4da8196d13d08049185f75c6860f68452
vickiedge <vicki.edge@my.jcu.edu.au>
2018-08-19 10:15:01 +0930

    security checker of list exercises complete

--- prac_04/list_excercises.py
+++ prac_04/list_excercises.py
@@ -10,3 +10,10 @@ print("The smallest number is", min(numbers))
 print("The largest number is", max(numbers))
 print("The average of the numbers is", sum(numbers)/len(numbers))
 
+#Security checker
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Enter username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
