072753c1d408d3a4d55a6df29745567d73a89bd8
vickiedge <vicki.edge@my.jcu.edu.au>
2018-10-12 09:34:48 +1030

    Update format prac_09

--- prac_09/file_sort.py
+++ prac_09/file_sort.py
@@ -1,5 +1,6 @@
 """
-Program to sort files with various name and extensions into subdirectories for each extension
+Program to sort files with various name and extensions into subdirectories for each extension\
+Version 1
 """
 
 import os
@@ -8,17 +9,11 @@ import shutil
 print("Starting directory is: {}".format(os.getcwd()))
 os.chdir('FilesToSort')
 print(os.getcwd())
-# os.mkdir('txt')
-# names = [name for name in os.listdir('.') if name.endswith('.txt')]
-extensions=[]
+extensions = []
 for name in os.listdir('.'):
-    # shutil.move(name, 'temp/' + name)
-    extension = name[name.find('.')+1:]
+    extension = name[name.find('.') + 1:]
     if extension not in extensions:
         extensions.append(extension)
         os.mkdir(extension)
-    shutil.move(name, extension+'/' + name)
+    shutil.move(name, extension + '/' + name)
     print(extension)
-
-
-
