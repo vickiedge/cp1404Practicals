041d01557ca3cbd3a1eb3077933a3c1bd1d276ff
vickiedge <vicki.edge@my.jcu.edu.au>
2018-10-14 16:42:58 +1030

    Attempt prac_09

--- prac_09/version2_file_sort.py
+++ prac_09/version2_file_sort.py
@@ -1,6 +1,7 @@
 """
 Program to sort files with various name and extensions into subdirectories for each extension\
-Version 1
+Version 2 - subdirectory folder names and extension sorting governed user input,
+stored in dictionary (key=extensions, value=folders)
 """
 
 import os
@@ -9,11 +10,27 @@ import shutil
 print("Starting directory is: {}".format(os.getcwd()))
 os.chdir('FilesToSort')
 print(os.getcwd())
-extension_to_folder = {}
+extensions = []
 for name in os.listdir('.'):
     extension = name[name.find('.') + 1:]
-    if extension not in extension_to_folder:
-        # extensions.append(extension) add to dictionary
-        os.mkdir(extension)
-    shutil.move(name, extension + '/' + name)
-    print(extension)
+    if extension not in extensions:
+        extensions.append(extension)
+categories = []
+for extension in extensions:
+    category = input("Wnat category would you like to sort {} files into? ".format(extension))
+    categories.append(category)
+#   print(extensions, categories)
+extension_to_category = dict(zip(extensions, categories))
+# print(extension_to_category)
+for name in os.listdir('.'):
+    if name[name.find('.') + 1:] == extension_to_category(extensions):
+         print(name, category)
+
+#     if extension not in extensions:
+#         extensions.append(extension)
+#         os.mkdir(extension)
+#     shutil.move(name, extension + '/' + name)
+#     print(extension)
+#
+# os.mkdir(categories)
+#
